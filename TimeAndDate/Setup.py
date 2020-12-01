1ec675a65abdaf44adefeb2648c897ba78377b59
Ezra Dulaney <dulaneyezra@gmail.com>
2020-12-01 12:27:10 -0500

    Update Setup.py

--- TimeAndDate/Setup.py
+++ TimeAndDate/Setup.py
@@ -1,11 +1,6 @@
 import os
 os.system("pip3 install ntplib")
-os.system("pip3 install termcolor")
-from termcolor import colored, cprint
-cprint("\nSuccessfully Installed Required Libraries", "green")
-question = input(colored("Would you like to update the date and time now? ", "red"))
-if question.lower() == "yes" or question.lower() == "y":
-    os.system("python3 ./TimeDate.py")
-else:
-    os.system("clear")
-    cprint(":( \nOkay run TimeDate.py at any time to update the computers date and time with the server", "red")
+os.system("clear")
+print("\nSuccessfully Installed Required Libraries")
+
+
